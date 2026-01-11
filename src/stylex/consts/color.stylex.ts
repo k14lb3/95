dd5a435b60d1bcbd3bea7896cb629e6ff21d1afe
k14lb3 <k14lb3@gmail.com>
2026-01-12 00:27:45 +0800

    💄 Add StyleX consts

---
+++ src/stylex/consts/color.stylex.ts
@@ -0,0 +1,16 @@
+import * as stylex from '@stylexjs/stylex';
+
+export const color = stylex.defineConsts({
+  transparent: 'transparent',
+  black: '#000000',
+  white: '#ffffff',
+  alto: '#dfdfdf',
+  silver: '#c0c0c0',
+  gray: '#808080',
+  red: '#ff0000',
+  maroon: '#800000',
+  pink: '#ff7f7f',
+  blue: '#000080',
+  yellow: '#ffff7f',
+  teal: '#008080',
+});
---
+++ src/stylex/consts/font.stylex.ts
@@ -0,0 +1,6 @@
+import * as stylex from '@stylexjs/stylex';
+
+export const font = stylex.defineConsts({
+  'Microsoft Sans Serif': '"Microsoft Sans Serif", monospace',
+  'IBM VGA 8x16': '"IBM VGA 8x16", monospace',
+});

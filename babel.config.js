cd3d39245f2ba2bd191eabcf464cfca13add135d
k14lb3 <k14lb3@gmail.com>
2026-01-13 18:33:32 +0800

    🔧 Remove redundant aliases

--- babel.config.js
+++ babel.config.js
@@ -17,10 +17,6 @@ export const plugins = [
         rootDir: dirname,
       },
       aliases: {
-        '@styles': [path.join(dirname, 'src/styles/index.css')],
-        '@components': [path.join(dirname, 'src/components/index.ts')],
-        '@hooks': [path.join(dirname, 'src/hooks/index.ts')],
-        '@lib': [path.join(dirname, 'src/lib/index.ts')],
         '@stylex/color.stylex.ts': [
           path.join(dirname, 'src/stylex/consts/color.stylex.ts'),
         ],

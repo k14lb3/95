0b814136ae5e2670458b60c2d06f1c34d4ac89ad
k14lb3 <k14lb3@gmail.com>
2026-01-19 00:15:19 +0800

    🔧 Route all paths to route

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 export default {
   reactCompiler: true,
+  rewrites: () => {
+    return [
+      {
+        source: '/:path*',
+        destination: '/',
+      },
+    ];
+  },
 };

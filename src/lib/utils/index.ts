523338f5eca836c9523e9047fa202efd3a4416ae
k14lb3 <k14lb3@gmail.com>
2026-02-16 10:07:49 +0800

    🔥 Remove unused util

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,7 +2,6 @@ export * from './generate-id';
 export * from './get-random-number';
 export * from './get-today-epoch-ms';
 export * from './get-viewport-to-design-scale';
-export * from './omit-undefined';
 export * from './open-url-in-new-tab';
 export * from './pxToVh';
 export * from './safe-parse';

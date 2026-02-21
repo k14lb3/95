c350dbc3f6ce5a8779f93d7b0ed5c0138cf52446
k14lb3 <k14lb3@gmail.com>
2026-02-21 18:37:44 +0800

    🚚 Create a folder named shared for shared types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,4 @@
 export * from './boot-stage';
 export * from './file-system-object';
-export * from './point';
-export * from './position';
-export * from './storage-value';
+export * from './shared/';
 export * from './utils';
